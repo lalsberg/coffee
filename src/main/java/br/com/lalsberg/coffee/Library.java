4d9e3b3a6ce52cc3f388101af829d32fee5e71a0
Leandro Alsberg <lalsberg@c934d7de.virtua.com.br>
2016-03-12 14:34:44 -0300

    Add Spring boot web

--- src/main/java/br/com/lalsberg/coffee/Library.java
+++ src/main/java/br/com/lalsberg/coffee/Library.java
@@ -6,6 +6,9 @@ package br.com.lalsberg.coffee;
  * @author lalsberg, @date 3/12/16 11:03 AM
  */
 public class Library {
+	public static void main(String[] args) {
+		System.out.println("started");
+	}
     public boolean someLibraryMethod() {
         return true;
     }
