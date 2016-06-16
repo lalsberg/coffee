9183938129a53384f609e04decf1a0ae4528f0cb
Leandro Alsberg <lalsberg@Leandros-MacBook-Pro.local>
2016-06-16 19:03:55 -0300

    Replaces jquery 3.0.0 with 2.2.4, compatible with bootstrap 3.3.6

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -6,12 +6,11 @@
 
 $("#btnLogin").click(function() {
 
-	$.post("http://localhost:8080/authenticate", 
-			{ 
-			  email : $("#email").val(), 
-			  password : $("#password").val() 
-			}, function(token) {
-				Cookies.set('jwtToken', token);
-				window.location.replace("/pages/index.html");
-			});
+	$.post("http://localhost:8080/authenticate", {
+		email : $("#email").val(),
+		password : $("#password").val()
+	}, function(token) {
+		Cookies.set('jwtToken', token);
+		window.location.replace("/pages/index.html");
+	});
 });
