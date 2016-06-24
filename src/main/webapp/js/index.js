a73121c3b592dac087fcac6fbad42440bca5893e
Leandro Alsberg <lalsberg@Leandros-MacBook-Pro.local>
2016-06-24 01:10:55 -0300

    refatora index.js

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -21,63 +21,6 @@ $(function() {
 
 		$("#orderedCoffees").append(itemsHtml);
 
-		$("#orderedCoffees").on("change", ".list-group-item", function() {
-//		$("#orderedCoffees .list-group-item").change(function() {
-			var coffeeId = $(this).find("input[name='coffeeId']").val();
-			var coffeeQuantityInput = $(this).find("input[name='coffeeQuantity']");
-			var coffeeName = $(this).find("p").text();
-			var coffeeQuantity = coffeeQuantityInput.val();
-
-			var coffeeOrder = {
-				coffee: {
-					id: coffeeId
-				},
-				quantity: coffeeQuantity
-			};
-
-			$.ajax("http://localhost:8080/club/1/orders/user/1", { 
-				data: JSON.stringify(coffeeOrder),
-				contentType : 'application/json', 
-				type : 'PUT', 
-				success: function(updatedCoffeeOrder) {
-					if(coffeeQuantity == 0) {
-						coffeeQuantityInput.parent().remove();
-
-						var removedCoffeeHtml = 
-							"<li class='list-group-item'>" +
-								"<input type='text' name='coffeeQuantity' value='0'></input> " + 
-								"<p>" + coffeeName + "</p>" +
-								"<input type='hidden' name='coffeeId' value='" + coffeeId + "'>" +
-							"</li>";
-
-						$("#coffees").append(removedCoffeeHtml);
-					}
-				}
-			});
-		});
-
-		$("#orderedCoffees").on("click", ".btn-danger", function() {
-			var removeButton = $(this);
-			var coffeeId = removeButton.siblings("input[name='coffeeId']").val();
-			var coffeeName = $(this).siblings("p").text();
-			var coffeeQuantity = $(this).siblings("input[name='coffeeQuantity']").val();
-
-			$.ajax("http://localhost:8080/club/1/orders/user/1/coffee/" + coffeeId, {
-				type : 'DELETE',
-				success: function() {
-					removeButton.parent().remove();
-
-					var removedCoffeeHtml = 
-						"<li class='list-group-item'>" +
-							"<input type='text' name='coffeeQuantity' value='0'></input> " + 
-							"<p>" + coffeeName + "</p>" +
-							"<input type='hidden' name='coffeeId' value='" + coffeeId + "'>" +
-						"</li>";
-
-					$("#coffees").append(removedCoffeeHtml);
-				}
-			});
-		});
 	});
 
 	$.get("http://localhost:8080/club/1/orders/user/1/unorderedCoffees", function(coffees) {
@@ -91,39 +34,95 @@ $(function() {
 				"</li>";
 		});
 		$("#coffees").append(itemsHtml);
+	});
 
-//		$("#coffees .list-group-item").change(function() {
-		$("#coffees").on("change", ".list-group-item", function() {
-			var coffeeId = $(this).find("input[name='coffeeId']").val();
-			var coffeeQuantityInput = $(this).find("input[name='coffeeQuantity']");
-			var coffeeName = $(this).find("p").text();
-			var coffeeQuantity = coffeeQuantityInput.val();
+	$("#coffees").on("change", ".list-group-item", function() {
+		var coffeeId = $(this).find("input[name='coffeeId']").val();
+		var coffeeQuantityInput = $(this).find("input[name='coffeeQuantity']");
+		var coffeeName = $(this).find("p").text();
+		var coffeeQuantity = coffeeQuantityInput.val();
 
-			var coffeeOrder = {
-				coffee: {
-					id: coffeeId
-				},
-				quantity: coffeeQuantity
-			};
+		var coffeeOrder = {
+			coffee: {
+				id: coffeeId
+			},
+			quantity: coffeeQuantity
+		};
 
-			$.ajax("http://localhost:8080/club/1/orders/user/1", { 
-				data: JSON.stringify(coffeeOrder),
-				contentType : 'application/json', 
-				type : 'POST', 
-				success: function(updatedCoffeeOrder) {
+		$.ajax("http://localhost:8080/club/1/orders/user/1", { 
+			data: JSON.stringify(coffeeOrder),
+			contentType : 'application/json', 
+			type : 'POST', 
+			success: function(updatedCoffeeOrder) {
+				coffeeQuantityInput.parent().remove();
+
+				var orderedCoffeeHtml = 
+					"<li class='list-group-item'>" +
+						"<input type='text' name='coffeeQuantity' value='" + coffeeQuantity + "'></input> " + 
+						"<p>" + coffeeName + "</p>" +
+						"<button type='button' class='btn btn-danger btn-block' style='width: 80px'>Remover</button>" +
+						"<input type='hidden' name='coffeeId' value='" + coffeeId + "'>" +
+					"</li>";
+
+				$("#orderedCoffees").append(orderedCoffeeHtml);
+			}
+		});
+	});
+
+	$("#orderedCoffees").on("change", ".list-group-item", function() {
+		var coffeeId = $(this).find("input[name='coffeeId']").val();
+		var coffeeQuantityInput = $(this).find("input[name='coffeeQuantity']");
+		var coffeeName = $(this).find("p").text();
+		var coffeeQuantity = coffeeQuantityInput.val();
+
+		var coffeeOrder = {
+			coffee: {
+				id: coffeeId
+			},
+			quantity: coffeeQuantity
+		};
+
+		$.ajax("http://localhost:8080/club/1/orders/user/1", { 
+			data: JSON.stringify(coffeeOrder),
+			contentType : 'application/json', 
+			type : 'PUT', 
+			success: function(updatedCoffeeOrder) {
+				if(coffeeQuantity == 0) {
 					coffeeQuantityInput.parent().remove();
 
-					var orderedCoffeeHtml = 
+					var removedCoffeeHtml = 
 						"<li class='list-group-item'>" +
-							"<input type='text' name='coffeeQuantity' value='" + coffeeQuantity + "'></input> " + 
+							"<input type='text' name='coffeeQuantity' value='0'></input> " + 
 							"<p>" + coffeeName + "</p>" +
-							"<button type='button' class='btn btn-danger btn-block' style='width: 80px'>Remover</button>" +
 							"<input type='hidden' name='coffeeId' value='" + coffeeId + "'>" +
 						"</li>";
 
-					$("#orderedCoffees").append(orderedCoffeeHtml);
+					$("#coffees").append(removedCoffeeHtml);
 				}
-			});
+			}
+		});
+	});
+
+	$("#orderedCoffees").on("click", ".btn-danger", function() {
+		var removeButton = $(this);
+		var coffeeId = removeButton.siblings("input[name='coffeeId']").val();
+		var coffeeName = $(this).siblings("p").text();
+		var coffeeQuantity = $(this).siblings("input[name='coffeeQuantity']").val();
+
+		$.ajax("http://localhost:8080/club/1/orders/user/1/coffee/" + coffeeId, {
+			type : 'DELETE',
+			success: function() {
+				removeButton.parent().remove();
+
+				var removedCoffeeHtml = 
+					"<li class='list-group-item'>" +
+						"<input type='text' name='coffeeQuantity' value='0'></input> " + 
+						"<p>" + coffeeName + "</p>" +
+						"<input type='hidden' name='coffeeId' value='" + coffeeId + "'>" +
+					"</li>";
+
+				$("#coffees").append(removedCoffeeHtml);
+			}
 		});
 	});
 
